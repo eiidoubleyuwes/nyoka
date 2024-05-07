b673c1e3521b30a4416f26b72ff8da6d769892f1
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-05-07 11:02:28 +0300

    Feat:First commit in a very long time

---
+++ refresher/day0.py
@@ -0,0 +1,13 @@
+#Here I am attempting to refresh my python knowledge
+#Lesson 1(Datatypes)
+age = 19
+age += 5
+print(age)
+print("Your age is: "+str(age))
+
+first_name = "Baraka"
+last_name = "Mbugua"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
