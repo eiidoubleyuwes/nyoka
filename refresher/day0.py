f2c9c8541413241352bdc4c8e32ce3421375f732
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-05-07 11:21:45 +0300

    Feat:Adding new lessons

--- refresher/day0.py
+++ refresher/day0.py
@@ -10,5 +10,16 @@ last_name = "Mbugua"
 full_name = first_name + " " + last_name
 print(full_name)
 
+#Lesson 2(Mulltiple Assignment)
+first_name, last_name = "Baraka", "Mnjala"
+
+#Lesson 3(String methods)
+job = "junior solutions architect"
+print(job.upper())
+print(job.lower())
+print(job.title())#Convert to title case
+print(job.count("s"))
+print(len(job))
+print(job.replace("Junior", "Senior"))
 
 
